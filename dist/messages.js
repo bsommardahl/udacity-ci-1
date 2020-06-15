77e5556c85d353fb61a54d9f0c33ee8caffcec8d
Byron Sommardahl <byron@sommardahl.com>
2020-06-15 14:30:40 -0500

    Cleaning up.

--- dist/messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateMessage = void 0;
-function generateMessage() {
-    return "Hello Byron";
-}
-exports.generateMessage = generateMessage;
--- dist/messages.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var messages_1 = require("./messages");
-test("generates the message correctly", function () {
-    var message = messages_1.generateMessage();
-    expect(message).toBe("Hello Allan");
-});
